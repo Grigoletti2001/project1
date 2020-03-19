f1267c18fb859c88f4d276afed596c9bc8cf9755
Joseph Grigoletti <jgrig2@gmail.com>
2020-03-19 12:48:33 -0500

    basic beginning functionality of app. html and css skeleton

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log(itza me mario); 
+//https://youtu.be/yJ6YPXlyTWY
+
+class Game {
+
+
+    constructor (player1, player2, player3, player4)  {
+
+        this.player1 = player1 ;
+        this.player2 = player2 ; 
+        this.player3 = player3 ; 
+        this.player4 = player4 ; 
+
+
+    }
+
+
+}
+
+const application {
+
+
+
+
+},
+
+
+
+
