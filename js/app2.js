f4df7b4e66710ef837f4af55bae8a6e2873c759b
Joseph Grigoletti <jgrig2@gmail.com>
2020-03-21 14:31:00 -0500

    updating code

--- js/app2.js
+++ js/app2.js
@@ -10,49 +10,50 @@ const application = {
     gameCharacters: ["Mario", "Peach", "Walugi", "Diddy-Kong"],
     cubeImages: ["assets/characters/image1.png", "assets/characters/image2.png", "assets/characters/image3.png", "assets/characters/image4.png"],
     gameImages: ["assets/characters/player1.png", "assets/characters/player2.png", "assets/characters/player3.png", "assets/characters/player4.png"],
-    currentImage: { index: 0 },
+    currentImage: { index1:0, index2:1, index3:2, index4:3, },
+
     cubeTime: function () {
         setInterval(function () {
-            console.log(application.cubeCharacters[application.currentImage.index]);
-            document.getElementById('divCubeCharacter').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+            console.log(application.cubeCharacters[application.currentImage.index1]);
+            document.getElementById('divCubeCharacter').innerHTML = `<img src="${application.cubeImages[application.currentImage.index1]
                 }" style="width: 300px; " />`;
-            application.currentImage.index++;
-            if (application.currentImage.index === application.cubeCharacters.length)
-                application.currentImage.index = 0;
+            application.currentImage.index1++;
+            if (application.currentImage.index1 === application.cubeCharacters.length)
+                application.currentImage.index1 = 0;
         }, 1000);
     },
 
     cubeTime2: function () {
         setInterval(function () {
-            console.log(application.cubeCharacters2[application.currentImage.index]);
-            document.getElementById('divCubeCharacter2').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+            console.log(application.cubeCharacters2[application.currentImage.index2]);
+            document.getElementById('divCubeCharacter2').innerHTML = `<img src="${application.cubeImages[application.currentImage.index2]
                 }" style="width: 300px; " />`;
-            application.currentImage.index++;
-            if (application.currentImage.index === application.cubeCharacters2.length)
-                application.currentImage.index = 0;
+            application.currentImage.index2++;
+            if (application.currentImage.index2 === application.cubeCharacters2.length)
+                application.currentImage.index2 = 0;
         }, 1000);
     },
 
 
     cubeTime3: function () {
         setInterval(function () {
-            console.log(application.cubeCharacters3[application.currentImage.index]);
-            document.getElementById('divCubeCharacter3').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+            console.log(application.cubeCharacters3[application.currentImage.index3]);
+            document.getElementById('divCubeCharacter3').innerHTML = `<img src="${application.cubeImages[application.currentImage.index3]
                 }" style="width: 300px; " />`;
-            application.currentImage.index++;
-            if (application.currentImage.index === application.cubeCharacters3.length)
-                application.currentImage.index = 0;
+            application.currentImage.index3++;
+            if (application.currentImage.index3 === application.cubeCharacters3.length)
+                application.currentImage.index3 = 0;
         }, 1000);
     },
 
     cubeTime4: function () {
         setInterval(function () {
-            console.log(application.cubeCharacters3[application.currentImage.index]);
-            document.getElementById('divCubeCharacter4').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+            console.log(application.cubeCharacters3[application.currentImage.index4]);
+            document.getElementById('divCubeCharacter4').innerHTML = `<img src="${application.cubeImages[application.currentImage.index4]
                 }" style="width: 300px; " />`;
-            application.currentImage.index++;
-            if (application.currentImage.index === application.cubeCharacters3.length)
-                application.currentImage.index = 0;
+            application.currentImage.index4++;
+            if (application.currentImage.index4 === application.cubeCharacters3.length)
+                application.currentImage.index4 = 0;
         }, 1000);
     },
 
