9a56049944d4615d11bb352fff7a77b2588554c6
Joseph Grigoletti <jgrig2@gmail.com>
2020-03-21 12:06:04 -0500

    basic version working

--- js/app2.js
+++ js/app2.js
@@ -3,6 +3,9 @@ console.log("itza me mario");
 const application = {
     //showing: 
     cubeCharacters: ["Bowser", "Toad", "Shell", "Boo"],
+    cubeCharacters2: ["Bowser", "Toad", "Shell", "Boo"],
+    cubeCharacters3: ["Bowser", "Toad", "Shell", "Boo"],
+    cubeCharacters4: ["Bowser", "Toad", "Shell", "Boo"],
     jumpSuccess: true,
     gameCharacters: ["Mario", "Peach", "Walugi", "Diddy-Kong"],
     cubeImages: ["assets/characters/image1.png", "assets/characters/image2.png", "assets/characters/image3.png", "assets/characters/image4.png"],
@@ -18,6 +21,44 @@ const application = {
                 application.currentImage.index = 0;
         }, 1000);
     },
+
+    cubeTime: function () {
+        setInterval(function () {
+            console.log(application.cubeCharacters2[application.currentImage.index]);
+            document.getElementById('divCubeCharacter2').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+                }" style="width: 300px; " />`;
+            application.currentImage.index++;
+            if (application.currentImage.index === application.cubeCharacters2.length)
+                application.currentImage.index = 0;
+        }, 1000);
+    },
+
+
+    cubeTime: function () {
+        setInterval(function () {
+            console.log(application.cubeCharacters3[application.currentImage.index]);
+            document.getElementById('divCubeCharacter3').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+                }" style="width: 300px; " />`;
+            application.currentImage.index++;
+            if (application.currentImage.index === application.cubeCharacters3.length)
+                application.currentImage.index = 0;
+        }, 1000);
+    },
+
+    cubeTime: function () {
+        setInterval(function () {
+            console.log(application.cubeCharacters3[application.currentImage.index]);
+            document.getElementById('divCubeCharacter4').innerHTML = `<img src="${application.cubeImages[application.currentImage.index]
+                }" style="width: 300px; " />`;
+            application.currentImage.index++;
+            if (application.currentImage.index === application.cubeCharacters3.length)
+                application.currentImage.index = 0;
+        }, 1000);
+    },
+
+
+
+
     displayGameCharacter: function () {
         for (let i = 0; i < application.gameCharacters.length; i++) {
             console.log(application.gameCharacters[i]);
