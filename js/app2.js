d6ad46cbc669ad73607acef90b92d1a37bf9388c
Joseph Grigoletti <jgrig2@gmail.com>
2020-03-23 00:03:41 -0500

    new mvp testing

--- js/app2.js
+++ js/app2.js
@@ -1,135 +1,81 @@
-console.log("itza me mario");
+// anonymous function calls itself
+(function () {
+    // shortcut for console.log
+    const log = console.log;
+    log("itza me mario");
 
-const application = {
-    //showing: 
-    cubeCharacters: ["Bowser", "Toad", "Shell", "Boo"],
-    cubeCharacters2: ["Bowser", "Toad", "Shell", "Boo"],
-    cubeCharacters3: ["Bowser", "Toad", "Shell", "Boo"],
-    cubeCharacters4: ["Bowser", "Toad", "Shell", "Boo"],
-    jumpSuccess: true,
-    gameCharacters: ["Mario", "Peach", "Walugi", "Diddy-Kong"],
-    gameScore: [0, 0, 0, 0],
-    cubeImages: ["assets/characters/image1.png", "assets/characters/image2.png", "assets/characters/image3.png", "assets/characters/image4.png"],
-    gameImages: ["assets/characters/player1.png", "assets/characters/player2.png", "assets/characters/player3.png", "assets/characters/player4.png"],
-    currentImage: { index1: 0, index2: 1, index3: 2, index4: 3, },
-
-    cubeTime: function () {
-        setInterval(function () {
-            //console.log(application.cubeCharacters[application.currentImage.index1]);
-            document.getElementById('divCubeCharacter').innerHTML = `<img src="${application.cubeImages[application.currentImage.index1]
-                }" style="width: 300px; " />`;
-            application.currentImage.index1++;
-            if (application.currentImage.index1 === application.cubeCharacters.length)
-                application.currentImage.index1 = 0;
-        }, 1000);
-    },
-
-    cubeTime2: function () {
-        setInterval(function () {
-            //console.log(application.cubeCharacters2[application.currentImage.index2]);
-            document.getElementById('divCubeCharacter2').innerHTML = `<img src="${application.cubeImages[application.currentImage.index2]
-                }" style="width: 300px; " />`;
-            application.currentImage.index2++;
-            if (application.currentImage.index2 === application.cubeCharacters2.length)
-                application.currentImage.index2 = 0;
-        }, 1000);
-    },
-
-
-    cubeTime3: function () {
-        setInterval(function () {
-            //console.log(application.cubeCharacters3[application.currentImage.index3]);
-            document.getElementById('divCubeCharacter3').innerHTML = `<img src="${application.cubeImages[application.currentImage.index3]
-                }" style="width: 300px; " />`;
-            application.currentImage.index3++;
-            if (application.currentImage.index3 === application.cubeCharacters3.length)
-                application.currentImage.index3 = 0;
-        }, 1000);
-    },
-
-    cubeTime4: function () {
-        setInterval(function () {
-            //console.log(application.cubeCharacters3[application.currentImage.index4]);
-            document.getElementById('divCubeCharacter4').innerHTML = `<img src="${application.cubeImages[application.currentImage.index4]
-                }" style="width: 300px; " />`;
-            application.currentImage.index4++;
-            if (application.currentImage.index4 === application.cubeCharacters3.length)
-                application.currentImage.index4 = 0;
-        }, 1000);
-    },
-
-
-
-
-    displayGameCharacter: function () {
-        for (let i = 0; i < application.gameCharacters.length; i++) {
-            //console.log(application.gameCharacters[i]);
-            document.getElementById('divGameCharacters').innerHTML += `<img src="${application.gameImages[i]}" style="float:left; margin-right: 2%;width: 300px;" />`;
-        }
+    let cubeImages = [];
+    for (let i = 0; i < 4; i++) {
+        cubeImages.push(`assets/characters/image${i + 1}.png`);
     }
-}
-//score keeping. which characters are above mario. 
-document.body.addEventListener("keydown", event => {
-    console.log(event);
-    if (event.code === "KeyA") {
-        // application.currentImage.index1 0,1,2,3 
-        if (application.currentImage.index1 == 1) {
-            // Toad is over Mario 
-            application.gameScore[0] += 1;
+    let gameImages = [];
+    for (let i = 0; i < 4; i++) {
+        gameImages.push(`assets/characters/player${i + 1}.png`);
+    }
 
-            if (application.gameScore[0] == 5) {
-                // Mario Wins
-                window.alert("Mario Wins");
+    let characters = ["Bowser", "Toad", "Shell", "Boo"];
+    let players = ["Mario", "Peach", "Walugi", "Diddy-Kong"];
+
+    const numOfCubes = 4;
+    let jumpSuccess = true;
+    let score = [0, 0, 0, 0];
+    const curImgIdx = [0, 1, 2, 3];
+
+    // random image indexes
+    // for (let i = 0; i < numOfCubes; i++) {
+    // 	curImgIdx.push(Math.round(Math.random() * cubeImages.length));
+    // }
+
+    for (let i = 0; i < numOfCubes; i++) {
+        setInterval(function () {
+            document.getElementById('cube' + i).innerHTML = `<img src="${cubeImages[curImgIdx[i]]
+                }" style="width: 300px; " />`;
+            // increment the curImgIdx for this cube
+            curImgIdx[i]++;
+            // set back to zero if it goes above the images length
+            if (curImgIdx[i] >= cubeImages.length) {
+                curImgIdx[i] = 0;
             }
+        }, 1000);
+    }
 
+    for (let i = 0; i < players.length; i++) {
+        //console.log(gameCharacters[i]);
+        document.getElementById('players').innerHTML += `<img src="${gameImages[i]}" style="float:left; margin-right: 2%;width: 300px;" />`;
+    }
 
-        }
-        else {
-            (application.currentImage.index1 !==1) {
-                application.gameScore[0]+= 0;
+    // score keeping. which characters are above mario.
+    document.body.addEventListener("keydown", event => {
+        console.log(event);
+        if (event.key == 'a') {
+            // currentImage.index1 0,1,2,3
+            if (currentImage.index1 == 1) {
+                // Toad is over Mario
+                score[0] += 1;
+
+                if (score[0] == 5) {
+                    // Mario Wins
+                    window.alert("Mario Wins");
+                }
+            } else if (currentImage.index1 !== 1) {
+                score[0] += 0;
             }
             // Toad is not equal to Mario (in terms ofbox rotation. )
         }
+        if (event.key == 'l') {
+            if (currentImage.index2 == 1) {
+                // Toad is over Peach
+                score[1] += 1;
 
-
-
-    }
-    if (event.code === "KeyL") {
-        if (application.currentImage.index2 == 1) {
-            // Toad is over Peach
-            application.gameScore[1] += 1;
-
-            if (application.gameScore[1] == 5) {
-                // Princess Peach Wins
-                window.alert("Princess Wins");
-            }
-
-        }
-        else {
-            // Toad is not over Peacch
-                (application.currentImage.index2 !==1) {
-                    application.gameScore[1]+=0; 
+                if (score[1] == 5) {
+                    // Princess Peach Wins
+                    window.alert("Princess Wins");
                 }
+            } else if (currentImage.index2 !== 1) {
+                // Toad is not over Peacch
+                score[1] += 0;
+            }
         }
-    }
-    
+    });
 
-
-
-
-
-});
-
-
-
-// Display the cube character
-application.cubeTime();
-application.cubeTime2();
-application.cubeTime3();
-application.cubeTime4();
-
-
-
-
-// Display the game character
-application.displayGameCharacter();
+})();
