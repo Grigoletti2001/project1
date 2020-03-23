8ca2417009a7619806eb02aa7e2959d9afacf6ba
Joseph Grigoletti <jgrig2@gmail.com>
2020-03-23 00:38:21 -0500

    test

--- js/app2.js
+++ js/app2.js
@@ -13,69 +13,82 @@
         gameImages.push(`assets/characters/player${i + 1}.png`);
     }
 
-    let characters = ["Bowser", "Toad", "Shell", "Boo"];
-    let players = ["Mario", "Peach", "Walugi", "Diddy-Kong"];
+    let cubeSideNames = ['Bowser', 'Toad', 'Shell', 'Boo'];
+    let playerNames = ['Mario', 'Peach', 'Walugi', 'Diddy-Kong'];
 
     const numOfCubes = 4;
     let jumpSuccess = true;
-    let score = [0, 0, 0, 0];
-    const curImgIdx = [0, 1, 2, 3];
+    let players = [];
+    for (let i = 0; i < playerNames.length; i++) {
+        players.push({
+            name: playerNames[i],
+            score: 0,
+            cubeSide: i
+        });
+    }
 
     // random image indexes
     // for (let i = 0; i < numOfCubes; i++) {
     // 	curImgIdx.push(Math.round(Math.random() * cubeImages.length));
     // }
 
+    // start cubes
     for (let i = 0; i < numOfCubes; i++) {
         setInterval(function () {
-            document.getElementById('cube' + i).innerHTML = `<img src="${cubeImages[curImgIdx[i]]
-                }" style="width: 300px; " />`;
             // increment the curImgIdx for this cube
-            curImgIdx[i]++;
+            players[i].cubeSide++;
             // set back to zero if it goes above the images length
-            if (curImgIdx[i] >= cubeImages.length) {
-                curImgIdx[i] = 0;
+            if (players[i].cubeSide >= cubeImages.length) {
+                players[i].cubeSide = 0;
             }
+            // replace cube image with the new cube side img
+            document.getElementById('cube' + i).innerHTML = `<img src="${cubeImages[players[i].cubeSide]
+                }" style="width: 300px; " />`;
         }, 1000);
     }
 
+    // add player character images
     for (let i = 0; i < players.length; i++) {
         //console.log(gameCharacters[i]);
         document.getElementById('players').innerHTML += `<img src="${gameImages[i]}" style="float:left; margin-right: 2%;width: 300px;" />`;
+    }
+
+    function stopCube(player) {
+        log(player.name + ' stopped cube at ' +
+            player.cubeSide + ' ' + cubeSideNames[player.cubeSide]);
+        // currentImage.index1 0,1,2,3
+        if (player.cubeSide == 1) {
+            // Toad is over Mario
+            player.score += 1;
+
+            if (player.score == 5) {
+                // Mario Wins
+                window.alert(player.name + ' Wins!');
+            }
+        } else {
+            player.score += 0;
+        }
+    }
+
+    function updateScores() {
+        for (let i = 0; i < players.length; i++) {
+            log(player.name + "'s score: " + player.score);
+        }
     }
 
     // score keeping. which characters are above mario.
     document.body.addEventListener("keydown", event => {
         console.log(event);
         if (event.key == 'a') {
-            // currentImage.index1 0,1,2,3
-            if (currentImage.index1 == 1) {
-                // Toad is over Mario
-                score[0] += 1;
-
-                if (score[0] == 5) {
-                    // Mario Wins
-                    window.alert("Mario Wins");
-                }
-            } else if (currentImage.index1 !== 1) {
-                score[0] += 0;
-            }
-            // Toad is not equal to Mario (in terms ofbox rotation. )
+            stopCube(players[0]);
+        } else if (event.key == 'f') {
+            stopCube(players[1]);
+        } else if (event.key == 'j') {
+            stopCube(players[2]);
+        } else if (event.key == 'l') {
+            stopCube(players[3]);
         }
-        if (event.key == 'l') {
-            if (currentImage.index2 == 1) {
-                // Toad is over Peach
-                score[1] += 1;
-
-                if (score[1] == 5) {
-                    // Princess Peach Wins
-                    window.alert("Princess Wins");
-                }
-            } else if (currentImage.index2 !== 1) {
-                // Toad is not over Peacch
-                score[1] += 0;
-            }
-        }
+        updateScores();
     });
 
 })();
